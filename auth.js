901ca6ce3c95129743ecb5db7fe8f0c568a19fde
feryardi70 <fery.ardiansyah94747@gmail.com>
2024-11-09 00:33:12 +0700

    missing secret

--- auth.js
+++ auth.js
@@ -39,6 +39,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     // authorized({ auth, request }) {
     //   const isLoggedIn = !!auth?.user;
